c9c46fd89287aa94933477dc3241ad4ca02f17f0
bouchrabenkhelifa <benbouchra317@gmail.com>
2025-03-25 22:34:03 +0100

    Home

--- app/src/main/java/com/example/medicall/ui/screens/Home.kt
+++ app/src/main/java/com/example/medicall/ui/screens/Home.kt
@@ -1,17 +1,19 @@
 package com.example.medicall.ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.medicall.ui.components.Header
+import com.example.medicall.ui.components.DoctorsList
+
 
 @Composable
 fun Home() {
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         Header(userName = "Bouchra")
         Spacer(modifier = Modifier.height(16.dp))
+        DoctorsList()
 
     }
 }
